fb8f6db2f2f4144080c31a193a4dd0e34cbb486a
Rathnakar <rathnakarn09@gmail.com>
2022-06-04 16:27:34 +0530

    url to asset

--- resources/views/headers/common_header.blade.php
+++ resources/views/headers/common_header.blade.php
@@ -6,25 +6,25 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Mission for Elimination of Poverty in Municipal Areas (MEPMA)</title>
-  <!--jquery--> 
+  <!--jquery-->
     <script src="{{ url('assets/js/jquery.min.js')}}"></script>
   <!-- plugins:css -->
-  <link rel="stylesheet" href="{{ url('assets/vendors/feather/feather.css')}}">
-  <link rel="stylesheet" href="{{ url('assets/vendors/ti-icons/css/themify-icons.css')}}">
-  <link rel="stylesheet" href="{{ url('assets/vendors/css/vendor.bundle.base.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/vendors/feather/feather.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/vendors/ti-icons/css/themify-icons.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/vendors/css/vendor.bundle.base.css')}}">
   <!-- endinject -->
   <!-- Plugin css for this page -->
-  <link rel="stylesheet" href="{{ url('assets/vendors/ti-icons/css/themify-icons.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/vendors/ti-icons/css/themify-icons.css')}}">
   <!-- End plugin css for this page -->
   <!-- inject:css -->
-  <link rel="stylesheet" href="{{ url('assets/css/vertical-layout-light/style20.css')}}">
-   <link rel="stylesheet" href="{{ url('assets/css/extracss1.css')}}">
-  <link rel="stylesheet" href="{{ url('assets/vendors/font-awesome/fontawesome-6.1.1/css/all.css')}}">
- <link rel="stylesheet" href="{{ url('assets/vendors/bootstrap5_icons/bootstrap-icons.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/css/vertical-layout-light/style20.css')}}">
+   <link rel="stylesheet" href="{{ asset('assets/css/extracss1.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/vendors/font-awesome/fontawesome-6.1.1/css/all.css')}}">
+ <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap5_icons/bootstrap-icons.css')}}">
   <!-- endinject -->
-  <link rel="shortcut icon" href="{{ url('assets/images/favicon.png')}}" />
+  <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png')}}" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-  
+
 </head>
 <style>
 
@@ -61,14 +61,14 @@
      display: none;
      background: rgba(0, 0, 0, 0.6);
  }
- 
+
  .cv-spinner {
      height: 100%;
      display: flex;
      justify-content: center;
      align-items: center;
  }
- 
+
  .spinner {
      width: 40px;
      height: 40px;
@@ -77,13 +77,13 @@
      border-radius: 50%;
      animation: sp-anime 0.8s infinite linear;
  }
- 
+
  @keyframes sp-anime {
      100% {
          transform: rotate(360deg);
      }
  }
- 
+
  .is-hide {
      display: none;
  }
@@ -265,8 +265,8 @@ input:read-only {
          @php
             Session::put('dashboard','dashboard_hrms');
          @endphp
-        <a class="navbar-brand brand-logo" href="{{ Session::get('dashboard'); }}"><img src="{{ url('assets/images/main_logo2.png')}}" class="mr-2" alt="logo"/></a>
-        <a class="navbar-brand brand-logo-mini" href="{{ Session::get('dashboard'); }}"><img src="{{ url('assets/images/mepma.png')}}" alt="logo"/></a>
+        <a class="navbar-brand brand-logo" href="{{ Session::get('dashboard'); }}"><img src="{{ asset('assets/images/main_logo2.png')}}" class="mr-2" alt="logo"/></a>
+        <a class="navbar-brand brand-logo-mini" href="{{ Session::get('dashboard'); }}"><img src="{{ asset('assets/images/mepma.png')}}" alt="logo"/></a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
         <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
@@ -277,42 +277,42 @@ input:read-only {
           <li class="nav-item nav-search d-none d-lg-block">
             <div class="input-group">
              <!--  Pattana Pragathi -->
-			  
+
             </div>
           </li>
         </ul>
         <ul class="navbar-nav navbar-nav-right">
-            
+
             	<!--Nofification Bell-->
 			<li class="nav-item dropdown">
-			    
+
 			    <a class="nav-link count-indicator dropdown-toggle notification" id="notificationDropdown" href="#" data-toggle="dropdown">
                   <!--<span>Inbox</span>-->
                   <i class="icon-bell mx-0"></i>
                   <span id="badge" ></span>
                 </a>
-			    
+
             <!--<a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">-->
             <!--  <i class="icon-bell mx-0 ringing">1</i>-->
             <!--  <span class="count"></span>-->
             <!--</a>-->
-            
+
             <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
               <p class="mb-0 font-weight-normal float-left dropdown-header">Notifications</p>
               <span id="notification_bar"></span>
-              
-              
-              
+
+
+
             </div>
           </li>
-			
+
           <li class="nav-item nav-profile dropdown">
             <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
-              <img src="{{ url('assets/images/faces/face28.jpg')}}" alt="profile"/>
+              <img src="{{ asset('assets/images/faces/face28.jpg')}}" alt="profile"/>
                <span class="name_prfle">{{ Session::get('user_id') }}</span>
             </a>
           </li>
-          
+
         </ul>
         <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
           <span class="icon-menu"></span>
@@ -340,90 +340,90 @@ input:read-only {
           </div>
         </div>
       </div>
-     
+
       <!-- partial -->
       <!-- partial:partials/_sidebar.html -->
-      
+
      @php
-     
+
       use App\Helper\Helpers;
       /*use Request;
       use Session;*/
-      
+
       /** checkin user authentication ***/
                  $userChk = new Helpers();
                  $result = $userChk->checkUser();
             /** close **/
-            
+
             /** get assigned menus **/
-            
-           
+
+
             $params = array(
                 'menu_project_id'=>Session::get('project_id'),
                 'user_id'=>Session::get('user_id')
                 );
-                
-                
+
+
             $services = $userChk->getAssignedMenus($params);
             if(count($services)<=0)
             {
             $services=array('0');
             }
-            
+
             /** check requested is assigned to the user or not ***/
-            
+
             $requested_uri = Request::path();
             $arr = explode('/',$requested_uri);
-            
-            
-            
+
+
+
             $assigned_services = $userChk->getURLAuthourization($services,$arr[0]);
-            
+
             if($assigned_services==0)
             {
                 echo "<div class='container'><div class='col-md-12'><div style='color:red; padding-top:8rem' class='text-center' > <br></br><br></br><h2>Your not a authorized user to view this page</h2></div></div></div>";
                 exit;
             }
-            
-    
-    
-      
-      
+
+
+
+
+
       foreach($services as $key=>$val)
       {
           if($val->show_in_menu_status==1)
           {
-            
-                
-            
-                
-                
+
+
+
+
+
                 $mainMenu[$val->menu_parent_id][$val->id]['menu_name'] = $val->menu_name;
                 $mainMenu[$val->menu_parent_id][$val->id]['menu_icon'] = $val->menu_icon;
                 $mainMenu[$val->menu_parent_id][$val->id]['menu_url'] = $val->menu_url;
                 $mainMenu[$val->menu_parent_id][$val->id]['id'] = $val->id;
                 $mainMenu[$val->menu_parent_id][$val->id]['has_dropdown'] = $val->has_dropdown;
-                
+
                 $subMenu[$val->menu_parent_id][$val->id]['menu_name'] = $val->menu_name;
                 $subMenu[$val->menu_parent_id][$val->id]['menu_icon'] = $val->menu_icon;
                 $subMenu[$val->menu_parent_id][$val->id]['menu_url'] = $val->menu_url;
             }
       }
-   
-      
+
+
       @endphp
-      
-      
-      
+
+
+
     <nav class="sidebar sidebar-offcanvas" id="sidebar">
-        
-        
-      
-        
+
+
+
+
         <ul class="nav">
-            
-             @foreach($mainMenu[0] as $parentid=>$mainmenudetails) 
-             
+
+             @foreach($mainMenu[0] as $parentid=>$mainmenudetails)
+
              @if($mainmenudetails['has_dropdown'] =='0')
                   <li class="nav-item">
                     <a class="nav-link {{ (request()->is($mainmenudetails['menu_url'])) ? 'myactive' : '' }}" href="{{ url($mainmenudetails['menu_url']) }}">
@@ -431,48 +431,48 @@ input:read-only {
                       <span class="menu-title">{{ $mainmenudetails['menu_name'] }}</span>
                     </a>
                   </li>
-              @else    
-                  
+              @else
+
                   <li class="nav-item">
                     <a class="nav-link" data-toggle="collapse" href="#ui-basic_{{ $parentid }}" aria-expanded="false" aria-controls="ui-basic_{{ $parentid }}">
                       <i class="{{ $mainmenudetails['menu_icon'] }}"></i>
                       <span class="menu-title">{{ $mainmenudetails['menu_name'] }}</span>
                       <i class="menu-arrow"></i>
                     </a>
-                    
+
                     <div class="collapse" id="ui-basic_{{ $parentid }}">
-                        
+
                       <ul class="nav flex-column sub-menu">
-                          @foreach($subMenu[$parentid] as $key=>$submenudetails) 
+                          @foreach($subMenu[$parentid] as $key=>$submenudetails)
                                 <li class="nav-item">
                                      <a class="nav-link {{ (request()->is($submenudetails['menu_url'])) ? 'myactive' : '' }}" href="{{ url($submenudetails['menu_url']) }}">{{ $submenudetails['menu_name'] }}</a>
                                 </li>
                           @endforeach
                       </ul>
-                      
+
                     </div>
                   </li>
-                  
+
                   @endif
-          
+
           @endforeach
-          
-           
+
+
           <li class="nav-item">
                     <a class="nav-link" href="{{url('logout')}}">
                       <i class="fas fa-sign-out-alt menu-icon"></i>
                       <span class="menu-title">Logout</span>
                     </a>
             </li>
-          
+
         </ul>
-        
-        
-        
-        
+
+
+
+
       </nav>
       <!-- partial -->
-      
+
       <script>
           $(document).ready(function(){
               $.ajax({
@@ -490,9 +490,8 @@ input:read-only {
               })
           })
       </script>
-      
-   
-      
-      
-      
-       
+
+
+
+
+
