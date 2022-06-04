e2455ea201ca57216c17069fab7ce4d504eeed12
Rathnakar <rathnakarn09@gmail.com>
2022-06-04 16:44:41 +0530

    URL TO ASSET IN FOOTER

--- resources/views/headers/footer.blade.php
+++ resources/views/headers/footer.blade.php
@@ -1,11 +1,11 @@
 <!-- content-wrapper ends -->
         <!-- partial:partials/_footer.html -->
-      
+
         <footer class="footer">
           <div class="d-sm-flex justify-content-center justify-content-sm-between">
             <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright ©. TS MEPMA
   All rights reserved.</span>
-            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Powered by <a href="https://vmaxindia.com/" target="blank"> <img src="{{ url('assets/images/vmax2.png')}}" width="80"></a> </span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Powered by <a href="https://vmaxindia.com/" target="blank"> <img src="{{ asset('assets/images/vmax2.png')}}" width="80"></a> </span>
           </div>
         </footer>
         <!-- partial -->
@@ -17,11 +17,11 @@
   <!-- container-scroller -->
 
   <!-- plugins:js -->
-  <script src="{{ url('assets/vendors/js/vendor.bundle.base.js')}}"></script>
+  <script src="{{ asset('assets/vendors/js/vendor.bundle.base.js')}}"></script>
   <!-- endinject -->
   <!-- Plugin js for this page -->
  <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
- 
+
  <script>
 $(document).ready(function(){
   $("#myInput").on("keyup", function() {
@@ -34,26 +34,26 @@ $(document).ready(function(){
 </script>
 
  <script src='https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js'></script>
-  
-  
+
+
 
 
   <!-- End plugin js for this page -->
   <!-- inject:js -->
-  <script src="{{ url('assets/js/off-canvas.js')}}"></script>
-  <script src="{{ url('assets/js/hoverable-collapse.js')}}"></script>
-  <script src="{{ url('assets/js/template.js')}}"></script>
-  <script src="{{ url('assets/js/settings.js')}}"></script>
-  <script src="{{ url('assets/js/todolist.js')}}"></script>
-    <script src="{{ url('assets/js/bootstrapbundle.min.js')}}"></script>
+  <script src="{{ asset('assets/js/off-canvas.js')}}"></script>
+  <script src="{{ asset('assets/js/hoverable-collapse.js')}}"></script>
+  <script src="{{ asset('assets/js/template.js')}}"></script>
+  <script src="{{ asset('assets/js/settings.js')}}"></script>
+  <script src="{{ asset('assets/js/todolist.js')}}"></script>
+    <script src="{{ asset('assets/js/bootstrapbundle.min.js')}}"></script>
   <!-- endinject -->
   <!-- Custom js for this page-->
-  <script src="{{ url('assets/js/dashboard.js')}}"></script>
-  <script src="{{ url('assets/js/Chart.roundedBarCharts.js')}}"></script>
-      <script src="{{ url('assets/js/jquery.table2excel.js')}}"></script>
+  <script src="{{ asset('assets/js/dashboard.js')}}"></script>
+  <script src="{{ asset('assets/js/Chart.roundedBarCharts.js')}}"></script>
+      <script src="{{ asset('assets/js/jquery.table2excel.js')}}"></script>
   <!-- End custom js for this page-->
-  
-  
+
+
   <link href="//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
 <link href="//cdn.datatables.net/buttons/1.5.6/css/buttons.bootstrap4.min.css" rel="stylesheet">
 
@@ -67,7 +67,7 @@ $(document).ready(function(){
 <script src="//cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
 <script src="//cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script>
 <script src="//cdn.datatables.net/buttons/1.5.6/js/buttons.colVis.min.js"></script>
-  
+
 
    <script>
       function exportTableToExcel(tableID, filename = ''){
@@ -75,15 +75,15 @@ $(document).ready(function(){
     var dataType = 'application/vnd.ms-excel';
     var tableSelect = document.getElementById(tableID);
     var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
-    
+
     // Specify file name
     filename = filename?filename+'.xls':'excel_data.xls';
-    
+
     // Create download link element
     downloadLink = document.createElement("a");
-    
+
     document.body.appendChild(downloadLink);
-    
+
     if(navigator.msSaveOrOpenBlob){
         var blob = new Blob(['\ufeff', tableHTML], {
             type: dataType
@@ -92,16 +92,16 @@ $(document).ready(function(){
     }else{
         // Create a link to the file
         downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
-    
+
         // Setting the file name
         downloadLink.download = filename;
-        
+
         //triggering the function
         downloadLink.click();
     }
 }
   </script>
-  
+
    <script>
   	$(".export-xl").click(function(){
   	    alert();
