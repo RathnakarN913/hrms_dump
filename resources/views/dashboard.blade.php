66df7ed5ddf44a14e2f435ba5b57f9ba6c48d36a
Rathnakar <rathnakarn09@gmail.com>
2022-06-04 16:42:31 +0530

    url to asset in dashboard blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,25 +22,25 @@
         <div class="content-wrapper p-4">
           <div class="row">
             <div class="col-md-12 ">
-               
+
             </div>
           </div>
-           
+
 
               <div class="row d-flex align-items-center">
 
                   @foreach($allProjects as $key=>$val)
-                 
+
                     <div class="mb-2  mbl_card">
                         <a href="{{ $val->external_project_url }}?q={{ Session::get('user_id') }}" target="_blank">
                     @if($val->is_external_dashboard )
                     @else
-                      <a href="{{ url('project-dashboard') }}/{{ $val->id }}">   
+                      <a href="{{ url('project-dashboard') }}/{{ $val->id }}">
                     @endif
                       <div class="card">
                         <div class="card-set d-flex  align-items-center justify-center text-center">
                             <div>
-                            <img src="{{ $val->project_icon }}">
+                            <img src="{{ asset($val->project_icon) }}">
                           <p class="mb-4 mt-2">{{ $val->project_name }}</p>
                           <p class="fs-30 mb-2"></p>
                           </div>
@@ -49,11 +49,11 @@
                       </div>
                       </a>
                     </div>
-                 
+
                 @endforeach
               </div>
 
-           
+
         </div>
         @include('headers.footer')
 
